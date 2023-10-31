ce5911c63ef37d7abe20b2d4de315581e115e79e
monikaBeizi <94218964+monikaBeizi@users.noreply.github.com>
2023-10-31 09:22:42 +0800

    1
    1

--- deeplearning/week4_5/Deep Neural Network Application - Image Classification/test.py
+++ deeplearning/week4_5/Deep Neural Network Application - Image Classification/test.py
@@ -7,11 +7,9 @@ from PIL import Image
 from scipy import ndimage
 from dnn_app_utils_v3 import *
 
-
-plt.rcParams['figure.figsize'] = (5.0, 4.0) # set default size of plots
+plt.rcParams['figure.figsize'] = (5.0, 4.0)  # set default size of plots
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
-
 
 np.random.seed(1)
 train_x_orig, train_y, test_x_orig, test_y, classes = load_data()
@@ -20,26 +18,27 @@ m_train = train_x_orig.shape[0]
 num_px = train_x_orig.shape[1]
 m_test = test_x_orig.shape[0]
 
-print ("Number of training examples: " + str(m_train))
-print ("Number of testing examples: " + str(m_test))
-print ("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
-print ("train_x_orig shape: " + str(train_x_orig.shape))
-print ("train_y shape: " + str(train_y.shape))
-print ("test_x_orig shape: " + str(test_x_orig.shape))
-print ("test_y shape: " + str(test_y.shape))
+print("Number of training examples: " + str(m_train))
+print("Number of testing examples: " + str(m_test))
+print("Each image is of size: (" + str(num_px) + ", " + str(num_px) + ", 3)")
+print("train_x_orig shape: " + str(train_x_orig.shape))
+print("train_y shape: " + str(train_y.shape))
+print("test_x_orig shape: " + str(test_x_orig.shape))
+print("test_y shape: " + str(test_y.shape))
 # Reshape the training and test examples
-train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0], -1).T   # The "-1" makes reshape flatten the remaining dimensions
+train_x_flatten = train_x_orig.reshape(train_x_orig.shape[0],
+                                       -1).T  # The "-1" makes reshape flatten the remaining dimensions
 test_x_flatten = test_x_orig.reshape(test_x_orig.shape[0], -1).T
 
 # Standardize data to have feature values between 0 and 1.
-train_x = train_x_flatten/255.
-test_x = test_x_flatten/255.
+train_x = train_x_flatten / 255.
+test_x = test_x_flatten / 255.
 
-print ("train_x's shape: " + str(train_x.shape))
-print ("test_x's shape: " + str(test_x.shape))
+print("train_x's shape: " + str(train_x.shape))
+print("test_x's shape: " + str(test_x.shape))
 
 ### CONSTANTS ###
-layers_dims = [12288, 20, 7, 5, 1] #  4-layer model
+layers_dims = [12288, 20, 7, 5, 1]  # 4-layer model
 
 
 # GRADED FUNCTION: L_layer_model
@@ -105,28 +104,29 @@ def L_layer_model(X, Y, layers_dims, learning_rate=0.0075, num_iterations=3000, 
     plt.show()
 
     return parameters
-parameters = L_layer_model(train_x, train_y, layers_dims, num_iterations = 2500, print_cost = True)
+
+
+parameters = L_layer_model(train_x, train_y, layers_dims, num_iterations=2500, print_cost=True)
 pred_train = predict(train_x, train_y, parameters)
 
 pred_test = predict(test_x, test_y, parameters)
 
 ## START CODE HERE ##
- # change this to the name of your image file
- # the true class of your image (1 -> cat, 0 -> non-cat)
+# change this to the name of your image file
+# the true class of your image (1 -> cat, 0 -> non-cat)
 ## END CODE HERE ##
 
 
-
-
 ## START CODE HERE ##
-my_image = "my_image.jpg" # change this to the name of your image file
-my_label_y = [1] # the true class of your image (1 -> cat, 0 -> non-cat)
+my_image = "my_image.jpg"  # change this to the name of your image file
+my_label_y = [1]  # the true class of your image (1 -> cat, 0 -> non-cat)
 ## END CODE HERE ##
 
 fname = "images/" + my_image
 image = np.array(plt.imread(fname))
-my_image = np.array(Image.fromarray(image).resize(size=(num_px,num_px))).reshape((num_px*num_px*3,1))
+my_image = np.array(Image.fromarray(image).resize(size=(num_px, num_px))).reshape((num_px * num_px * 3, 1))
 my_predicted_image = predict(my_image, my_label_y, parameters)
 
 plt.imshow(image)
-print ("y = " + str(np.squeeze(my_predicted_image)) + ", your L-layer model predicts a \"" + classes[int(np.squeeze(my_predicted_image)),].decode("utf-8") +  "\" picture.")
+print("y = " + str(np.squeeze(my_predicted_image)) + ", your L-layer model predicts a \"" + classes[
+    int(np.squeeze(my_predicted_image)),].decode("utf-8") + "\" picture.")
